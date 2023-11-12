890fa2375ee7093ad1708566bde9ab095f90584b
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2023-11-12 13:50:10 -0500

    :sparkles: feat: Added HTTP mirror behaivor.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# python-3-flask-http-mirror
-This project primarily serves as a diagnostic tool for HTTP request inspection. t mirrors back every detail of HTTP requests sent to it, including request headers, body, and method, providing a clear view of incoming requests.
+# Python-3-Flask-HTTP-Mirror 🌐🔍
+
+## Description
+
+**Python-3-Flask-HTTP-Mirror** is a minimalistic yet powerful microservice developed using Python 3 and Flask. This project primarily serves as a diagnostic tool for HTTP request inspection. It mirrors back every detail of HTTP requests sent to it, including request headers, body, and method, providing a clear view of incoming requests. 🔄
+
+This microservice is particularly useful for developers and testers who need to debug or examine the contents of HTTP requests in various scenarios, such as API development, webhook testing, and HTTP client configuration. By echoing back the exact request details, it aids in understanding how requests are being received and interpreted by the server. 🕵️‍♂️👩‍💻
+
+### Key Features:
+
+- **Request Reflection:** Mirrors all aspects of incoming HTTP requests. 📬
+- **Support for Various HTTP Methods:** Compatible with GET, POST, PUT, DELETE, and more. 📌
+- **JSON Response Format:** Returns data in an easily readable JSON format. 📊
+- **Lightweight and Easy to Deploy:** Utilizing Flask makes it easy to set up and run in any environment. 💼
+- **Docker Integration:** Comes with Dockerfile for quick containerization and deployment. 🐳
+
+### How to Use:
+
+Deploy the microservice using Flask or Docker, and send any HTTP request to it. The service will return a JSON response containing the method, headers, and body of the request. This response can be used for debugging, testing, or any other purpose where request inspection is needed. 🚀
+
+### Ideal for:
+
+- Developers working on REST APIs. 👨‍💻👩‍💻
+- Testing webhooks and HTTP clients. 🧪
+- Debugging and development environments. 🐛
+
+**Python-3-Flask-HTTP-Mirror** is an excellent tool for anyone looking to gain more insight into the HTTP requests their applications are handling, simplifying the process of debugging and testing in web development. 🌟
---
+++ app.py
@@ -0,0 +1,71 @@
+# MIT License
+# 
+# COPYRIGHT (c) 2023 Jorge Garcia
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from datetime import datetime, timezone
+from flask import Flask, request, jsonify
+import tzlocal
+
+app = Flask(__name__)
+
+@app.route('/mirror', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def http_mirror():
+    """
+    Returns a JSON object containing information about the incoming HTTP request.
+
+    Returns:
+        A JSON object containing the following information:
+        - datetime: The current date and time in ISO format.
+        - method: The HTTP method used in the request.
+        - url: The URL of the request.
+        - headers: A dictionary containing the headers of the request.
+        - args: A dictionary containing the query string parameters of the request.
+        - form: A dictionary containing the form data of the request.
+        - data: The raw data of the request.
+        - cookies: A dictionary containing the cookies of the request.
+        - files: A dictionary containing the files uploaded in the request.
+        - json: The JSON data of the request.
+        - referrer: The referrer of the request.
+        - remote_addr: The IP address of the client making the request.
+        - scheme: The URL scheme of the request.
+        - user_agent: The user agent of the client making the request.
+        - environ: A dictionary containing the environment variables of the request.
+    """
+    return jsonify({
+        'datetime': datetime.now(tzlocal.get_localzone()).isoformat(),
+        'method': request.method,
+        'url': request.url,
+        'headers': dict(request.headers),
+        'args': dict(request.args),
+        'form': dict(request.form),
+        'data': request.data.decode('utf-8'),
+        'cookies': request.cookies,
+        'files': dict(request.files),
+        'json': request.json,
+        'referrer': request.referrer,
+        'remote_addr': request.remote_addr,
+        'scheme': request.scheme,
+        'user_agent': request.user_agent.string,
+        'environ': {k: v for k, v in request.environ.items() if isinstance(v, (str, int, float, list, dict, bool))},
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
